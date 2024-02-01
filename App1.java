3732607fe775f360a4d5eb40fef6ff49ba24c8e5
rsmadhavan <rsmadhavan2000@gmail.com>
2024-02-01 12:25:56 +0530

    Made App better

--- App1.java
+++ App1.java
@@ -2,5 +2,7 @@ public class App1{
 
   public static void main(String[] args){
     System.out.println("Java App Pulled & Build by Jenkins");
+    System.out.println("Zaminamina Zangalewa! This time for AFRICAAAAA!!!!!!!! <3");
   }
+
 }
