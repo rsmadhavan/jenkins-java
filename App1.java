89415eaae578707edc7c5d7d8e330093f09286b5
rsmadhavan <rsmadhavan2000@gmail.com>
2024-02-01 15:42:56 +0530

    webhook test

--- App1.java
+++ App1.java
@@ -3,7 +3,7 @@ public class App1{
   public static void main(String[] args){
     System.out.println("Java App Pulled & Build by Jenkins");
     System.out.println("Zaminamina Zangalewa! This time for AFRICAAAAA!!!!!!!! <3");
-    System.out.println("Shakira test webhook");
+   
   }
 
 }
